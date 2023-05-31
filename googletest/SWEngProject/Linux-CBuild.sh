8d8cf5e583e0a4ad773d4264346b2de22451a440
FilippoIspanico <133004373+FilippoIspanico@users.noreply.github.com>
2023-05-31 17:02:54 +0200

    Update Linux-CBuild.sh

--- googletest/SWEngProject/Linux-CBuild.sh
+++ googletest/SWEngProject/Linux-CBuild.sh
@@ -8,5 +8,5 @@ cd /usr/lib/x86_64-linux-gnu/
 sudo cp libgtest.a libgtest_main.a /usr/lib
 
 cd $STARTING_PATH
-g++ -o calc_test calc_test.cpp calc.cpp -lgtest -lpthread
+# g++ -o calc_test calc_test.cpp calc.cpp -lgtest -lpthread
 
