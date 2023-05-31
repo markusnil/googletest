8ca9ca0295f03c114ceb528a357aa390f9d7d246
FilippoIspanico <133004373+FilippoIspanico@users.noreply.github.com>
2023-05-31 17:13:22 +0200

    Update Linux-CBuild.sh

--- googletest/SWEngProject/Linux-CBuild.sh
+++ googletest/SWEngProject/Linux-CBuild.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 export STARTING_PATH=$(pwd)
-sudo apt install libgtest-dev
-sudo apt install cmake
-sudo apt install g++
 cd /usr/src/gtest && sudo cmake CMakeLists.txt && sudo make
 cd /usr/lib/x86_64-linux-gnu/
 sudo cp libgtest.a libgtest_main.a /usr/lib
